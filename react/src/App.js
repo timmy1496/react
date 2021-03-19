a113a631f2f16fbc5770e1090cad5a662ab871c6
chernenko <artem.chernenko1496@gmail.com>
2021-03-19 16:19:17 +0200

    finished lesson 10

---
+++ react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Footer from './Footer';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Technologies/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div>
+            <a href="">Links</a>
+            <a href="">Road map</a>
+            <a href="">Test</a>
+        </div>
+    );
+}
+
+export default Footer;
