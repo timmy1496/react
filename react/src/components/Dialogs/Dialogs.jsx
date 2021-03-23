3a9aa5bdf82fc166a2993dd7d3d6a3f69a6d6aa8
chernenko <artem.chernenko1496@gmail.com>
2021-03-23 13:20:58 +0200

    finish lesson 21

--- react/src/components/Dialogs/Dialogs.jsx
+++ react/src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,39 @@
+import s from './Dialogs.module.css';
+
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     );
 }
