45aa792bfbcfa26d6a7d3bc6dcbab119892670df
chernenko <artem.chernenko1496@gmail.com>
2021-03-29 06:38:12 +0300

    finished lesson 32

--- react/src/App.js
+++ react/src/App.js
@@ -24,7 +24,8 @@ const App = (props) => {
                                state={props.state.dialogsPage} />}/>
                     <Route path='/profile'
                            render={() => <Profile
-                               state={props.state.profilePage} />} />
+                               state={props.state.profilePage}
+                               addPost={ props.addPost }/>} />
                     <Route path='/news' render={() => <News/>} />
                     <Route path='/music' render={() => <Music/>} />
                 </div>
