f2e2423a584b692229b7ac5772b62682bc87d6f9
chernenko <artem.chernenko1496@gmail.com>
2021-03-29 06:00:40 +0300

    finished lesson 31

--- react/src/components/Dialogs/Dialogs.jsx
+++ react/src/components/Dialogs/Dialogs.jsx
@@ -3,12 +3,19 @@ import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = function (props) {
 
     let dialogsElemets = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
 
     let messages = props.state.messages.map(message => <Message message={message.message}/>);
+
+    let newMessage = React.createRef()
+
+    let addMessage = () => {
+        alert(newMessage.current.value)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -17,6 +24,10 @@ const Dialogs = function (props) {
             </div>
             <div className={s.messages}>
                 { messages}
+            </div>
+            <div>
+                <textarea ref={ newMessage } name="" id="" cols="30" rows="10"></textarea>
+                <button onClick={ addMessage }>Add message</button>
             </div>
         </div>
     );
--- react/src/components/Profile/MyPosts/MyPosts.jsx
+++ react/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,28 @@
-import s from './MyPosts.module.css';
+
 import Post from './Post/Post';
+import React from "react";
+import s from './MyPosts.module.css';
 
 const MyPosts = (props) => {
+
     let posts = props.posts.map(post => <Post message={post.message} likeCount={post.likeCount}/>);
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>my posts</h3>
             <div>
                 <div>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <textarea ref={ newPostElement } name="" id="" cols="30" rows="10"></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
                 <div>
                     <button>Remove</button>
