a9ac9c51c4135d4e4ac2724582b45eb901b2e02e
chernenko <artem.chernenko1496@gmail.com>
2021-04-16 14:53:57 +0300

    finished lesson 52

--- react/src/components/Users/Users.jsx
+++ react/src/components/Users/Users.jsx
@@ -4,59 +4,14 @@ import userPhoto from '../../assets/images/user.jpg'
 
 const Users = (props) => {
 
-    let users = [
-        {
-            id: 1,
-            followed: false,
-            fullName: 'Dmitry K.',
-            location: {
-                country: 'Belarus',
-                city: 'Minsk',
-            },
-            status: 'I am looking for great job now',
-            avatar: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_DE/viacom-358518-Full-Image_GalleryBackground-de-DE-1501272675282._V7ea3c5f3b475ec3e9224ddb482756b00_SX1080_.jpg',
-        },
-        {
-            id: 2,
-            followed: true,
-            fullName: 'Svetlana D.',
-            location: {
-                country: 'Belarus',
-                city: 'Pinsk',
-            },
-            status: 'I am so pretty',
-            avatar: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_DE/viacom-358518-Full-Image_GalleryBackground-de-DE-1501272675282._V7ea3c5f3b475ec3e9224ddb482756b00_SX1080_.jpg',
-        },
-        {
-            id: 3,
-            followed: false,
-            fullName: 'Sergei S.',
-            location: {
-                country: 'Ukraine',
-                city: 'Kiev',
-            },
-            status: 'I am the best',
-            avatar: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_DE/viacom-358518-Full-Image_GalleryBackground-de-DE-1501272675282._V7ea3c5f3b475ec3e9224ddb482756b00_SX1080_.jpg',
-        },
-        {
-            id: 4,
-            followed: false,
-            fullName: 'Andrew T.',
-            location: {
-                country: 'Ukraine',
-                city: 'Dnipro',
-            },
-            status: 'hah. rofl',
-            avatar: 'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_DE/viacom-358518-Full-Image_GalleryBackground-de-DE-1501272675282._V7ea3c5f3b475ec3e9224ddb482756b00_SX1080_.jpg',
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
         }
-    ];
-
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items);
-        });
-    }
+    };
 
     const follow = (userID) => {
         props.follow(userID)
@@ -68,6 +23,7 @@ const Users = (props) => {
 
     return (
         <div className={classes}>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u =>
                     <div key={u.id}>
