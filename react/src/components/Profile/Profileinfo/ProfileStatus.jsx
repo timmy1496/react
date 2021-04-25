d0ba9c77ae4e13773585667718da29254c275309
chernenko <artem.chernenko1496@gmail.com>
2021-04-25 19:11:38 +0300

    finish lesson 74

--- react/src/components/Profile/Profileinfo/ProfileStatus.jsx
+++ react/src/components/Profile/Profileinfo/ProfileStatus.jsx
@@ -27,6 +27,16 @@ class ProfileStatus extends React.Component {
         });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status,
+            });
+        }
+
+        console.log('componentDidUpdate');
+    }
+
     render() {
         return (
             <div>
